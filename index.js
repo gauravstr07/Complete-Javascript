cb85706c78282ec3eafe2f9f5be782e6483d1707
Gaurav Sutar <83762168+gauravstr07@users.noreply.github.com>
2022-11-23 13:18:30 +0530

    Objects PART 4

--- index.js
+++ index.js
@@ -1,26 +1,10 @@
-const vahicle = {
-  wheels: 4,
-  engine: function () {
-    return "Vrooooommm...";
-  },
-  model: {
-    color: "Red",
-    brand: "Tata",
-  },
-};
-
-const truck = Object.create(vahicle);
-truck.door = 2;
-//console.log(truck.model);
-
-const car = Object.create(vahicle);
-car.door = 4;
-car.model = {
+const OnePlus = {
+  model: "OnePlus Nord",
+  ram: "12GB",
+  rom: "256GB",
+  price: 29956,
   color: "Black",
-  brand: "Ferrari",
 };
-car.engine = function () {
-  return "Booommmbooomm";
-};
-console.log(car.engine());
 
+const { ram: myVariable } = OnePlus;
+console.log(myVariable);
